a1ab1cff272d3b6be33955c5876e0cb90b7b0239
Lockig <80728810+Lockig@users.noreply.github.com>
2021-10-24 20:18:42 +0700

    Add files via upload
    File các bảng sql và dữ liệu

---
+++ Data_QL_Diem_SV.sql
@@ -0,0 +1,77 @@
+﻿use QL_DIEM_SV
+--PHẦN DỮ LIỆU
+--BẢNG LOGIN
+INSERT INTO LOGIN VALUES('admin','password')
+
+--BẢNG KHOA
+INSERT INTO KHOA VALUES 
+	('KHMT','Khoa hoc may tinh','P1002','0123456788',2008),
+	('DT','Dau tu','P1003','0123456787',2008),
+	('CNTT','Cong nghe thong tin','P1000','0123456789',2008),
+	('QTKD','Quan tri kinh doanh','P1001','0123456787',1980)
+delete KHOA
+use master
+--BẢNG MONHOC
+INSERT INTO MONHOC VALUES 
+	('CNTT1','Nhap mon cong nghe thong tin','CNTT',3),
+	('CNTT2','Lap trinh C++','CNTT',3),
+	('KHMT1','Tri tue nhan tao','KHMT',3),
+	('KHMT2','Lap trinh khoa hoc du lieu','KHMT',3),
+	('QTKD1','Marketing can ban','QTKD',3),
+	('QTKD2','Marketing nang cao','QTKD',3),
+	('DT1','Nhap mon dau tu','DT',3),
+	('DT2','Quan tri nhan luc','DT',3)
+delete MONHOC
+--Bảng giangvien
+insert into GIANGVIEN values
+	('GV1','Dang Minh Quan',1,'1977-1-1','0912345678','Ha Noi','CNTT'),
+	('GV2','Cao Thi Thu Huong',0,'1985-1-1','0912345676','Nam Dinh','CNTT'),
+	('GV3','Luu Minh Tuan',1,'1980-1-1','0912345672','Nam Dinh','KHMT'),
+	('GV4','Pham Minh Hoan',1,'1979-1-1','0912345674','Nam Dinh','CNTT'),
+	('GV5','Nguyen Thi Chinh',0,'1985-1-1','0912345666','Vinh Phuc','QTKD'),
+	('GV6','To Thi Thien Huong',0,'1982-1-1','0912345665','Bac Ninh','QTKD'),
+	('GV7','Nguyen Minh Ngoc',0,'1977-1-1','0912345662','Nghe An','DT'),
+	('GV8','Nguyen Thi Chinh',0,'1985-1-1','0912345661','Thanh Hoa','DT')
+--BẢNG LOPCQ
+INSERT INTO LOPCQ VALUES
+	('CNTT61A','Cong nghe thong tin 61A','CNTT',50),
+	('KHMT61','Khoa hoc may tinh 61','KHMT',50),
+	('DT58','Dau tu 58','DT',50),
+	('QTKD59B','Quan tri kinh doanh 59B','QTKD',50)
+--Bảng giangvien.monhoc
+insert into GIANGVIEN_MONHOC values
+	('CNTT1','GV1'),
+	('CNTT2','GV2'),
+	('KHMT1','GV3'),
+	('KHMT2','GV4'),
+	('QTKD1','GV5'),
+	('QTKD2','GV6'),
+	('DT1','GV7'),
+	('DT2','GV8')
+--Bảng lophocphan
+insert into LOPHOCPHAN values
+	('CNTT1_1','Nhap mon cong nghe thong tin(220)_01','CNTT1','GV1',50),
+	('CNTT2_1','Lap trinh C++(220)_01','CNTT2','GV2',50),
+	('KHMT1_1','Tri tue nhan tao','KHMT1','GV3',50),
+	('KHMT2_1','Lap trinh khoa hoc du lieu(219)_01','KHMT2','GV4',50),
+	('QTKD1_1','Marketing can ban(219)_01','QTKD1','GV5',50),
+	('DT1_1','Nhap mon dau tu(221)_01','DT1','GV7',50),
+	('DT2_1','Quan tri nhan luc(221)_01','DT2','GV8',50)
+--Bang diemhocphan
+insert into DIEMHOCPHAN values
+(),
+--BẢNG SINHVIEN
+--Bang chungchi
+insert into CHUNGCHI values
+('CC1','Chung chi tin hoc'),
+('CC2','Chung chi tieng anh'),
+('CC3','Chung chi quoc phong')
+---------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
+--SELECT DỮ LIỆU
+select *from LOGIN
+select *from KHOA
+select *from MONHOC
+select *from LOPHOC
+select *from SINHVIEN
+select *from DIEMHOCPHAN
---
+++ QL_DIEM_SV.sql
@@ -0,0 +1,97 @@
+﻿CREATE DATABASE QL_DIEM_SV
+use master
+USE QL_DIEM_SV
+drop database QL_DIEM_SV
+--------------------------------------------------------
+--------------------------------------------------------
+--Tạo bảng LOGIN
+create table LOGIN(
+	user_id nvarchar(50) primary key,
+	password nvarchar(50)
+)
+--Tạo bảng KHOA
+create table KHOA(
+	id_khoa char(10) primary key not null,
+	ten_khoa nvarchar(50),
+	dia_chi nvarchar(50),
+	so_dien_thoai varchar(10) ,
+	nam_thanh_lap int check (nam_thanh_lap <= 2021  and nam_thanh_lap >=1960)
+)
+--Tạo bảng MONHOC
+create table MONHOC(
+	id_mon char(10) primary key not null,
+	ten_mon nvarchar(50),
+	id_khoa char(10) references KHOA(id_khoa),
+	so_tin_chi int check(so_tin_chi in ('2','3','10','8'))
+)
+--Tạo bảng giangvien
+create table GIANGVIEN
+(	id_GV char(10) primary key,
+	ten_GV nvarchar(50),
+	gioi_tinh bit,
+	dob_gv date,
+	So_dien_thoai char(10),
+	quequan char(20),
+	id_khoa char(10) references KHOA(id_khoa)
+)
+--Tạo bảng giangvien.monhoc
+create table GIANGVIEN_MONHOC(
+	id_mon char(10) references MONHOC(id_mon),
+	id_gv char(10) references GIANGVIEN(id_gv)
+	primary key(id_mon,id_gv)
+)
+drop table GIANGVIEN_MONHOC
+--Tạo bảng LOPHOCPHAN
+create table LOPHOCPHAN(
+	id_lhp char(10) primary key,
+	ten_lhp nvarchar(50),
+	id_mon char(10),
+	id_gv char(10),
+	constraint FK_GV_MON foreign key(id_mon,id_gv) references GIANGVIEN_MONHOC(id_mon,id_gv),
+	si_so int check(si_so <=60)
+)
+--Tạo bảng LOPHOC
+create table LOPCQ(
+	id_lop char(10) primary key,
+	ten_lop nvarchar(50),
+	id_khoa char(10) references KHOA(id_khoa),
+	si_so int check(si_so <=60)
+)
+--Tạo bảng SINHVIEN
+create table SINHVIEN(
+	id_sv char(10) primary key,
+	ten_sv nvarchar(50),
+	dob_sv date,
+	gioi_tinh bit,
+	que_quan nvarchar(50),
+	id_lop char(10) references LOPCQ(id_lop)
+)
+--Tạo bảng DIEMHOCPHAN
+create table DIEMHOCPHAN(
+	id_lhp char(10) references LOPHOCPHAN(id_lhp),
+	id_sv char(10) references SINHVIEN(id_sv),
+	primary key (id_sv,id_lhp),
+	diem_cc float check(diem_cc > 0 and diem_cc <= 10),
+	diem_gk float check(diem_gk > 0 and diem_gk <= 10),
+	diem_ck float check(diem_ck > 0 and diem_ck <= 10),
+	diem_tk float default null
+)
+--Tạo bảng Chứng chỉ
+create table CHUNGCHI(
+	id_cc char(10) primary key,
+	ten_cc nvarchar(50)
+)
+create table SINHVIEN_CHUNGCHI(
+	id_sv char(10) references SINHVIEN(id_sv),
+	id_cc char(10) references CHUNGCHI(id_cc),
+	primary key(id_sv,id_cc)
+)
+----Tạo bảng Chương trình đào tạo
+--create table CTDT(
+--	id_khoa char(10) references Khoa(id_khoa),
+--	id_lop char(10) references Lophoc(id_lop),
+--	id_mon char(10) references MONHOC(id_mon),
+--	so_tin_chi int references TINCHI(so_tin_chi),
+	
+--)
+--Tạo bảng DIEM_SINH_VIEN
